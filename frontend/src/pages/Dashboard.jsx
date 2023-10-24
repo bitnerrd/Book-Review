e2c137aa75e8df796cf5fee0d3e7b4ac725ac629
bitnerrd <irfanabid.913@gmail.com>
2023-10-24 18:01:05 +0500

    add-book

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,155 @@
-import React from "react";
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import "../assests/styles/dashboard.css";
 
 const Dashboard = () => {
-  return <div>Dashboad</div>;
+  const [books, setBooks] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [newBook, setNewBook] = useState({
+    title: "",
+    author: "",
+    publishYear: "",
+    edition: "",
+    price: "",
+  });
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [selectedBookIndex, setSelectedBookIndex] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewBook((prevBook) => ({
+      ...prevBook,
+      [name]: value,
+    }));
+  };
+
+  const addOrUpdateBook = () => {
+    if (isEditMode) {
+      const updatedBooks = [...books];
+      updatedBooks[selectedBookIndex] = newBook;
+      setBooks(updatedBooks);
+    } else {
+      setBooks([...books, newBook]);
+    }
+    resetForm();
+  };
+
+  const editBook = (index) => {
+    setSelectedBookIndex(index);
+    setNewBook(books[index]);
+    setIsEditMode(true);
+    setShowModal(true);
+  };
+
+  const resetForm = () => {
+    setNewBook({
+      title: "",
+      author: "",
+      publishYear: "",
+      edition: "",
+      price: "",
+    });
+    setShowModal(false);
+    setIsEditMode(false);
+    setSelectedBookIndex(null);
+  };
+
+  return (
+    <div>
+      <Button onClick={() => setShowModal(true)} className="add-book">
+        Add Book
+      </Button>
+      <div className="container-fluid mt-3">
+        <div className="row">
+          {books.map((book, index) => (
+            <div key={index} className="col-12 col-md-6 col-lg-4 mb-4">
+              <div className="card">
+                <div
+                  className="card-body"
+                  style={{ backgroundColor: "#F4F5FF" }}
+                >
+                  <h5 className="card-title">{book.title}</h5>
+                  <p className="card-text">By: {book.author}</p>
+                  <p className="card-text">Published on: {book.publishYear}</p>
+                  <p className="card-text">Edition: {book.edition}</p>
+                  <p className="card-text">Price: ${book.price}</p>
+                  <Button onClick={() => editBook(index)}>Edit Book</Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <Modal show={showModal} onHide={resetForm}>
+        <Modal.Header closeButton>
+          <Modal.Title>{isEditMode ? "Edit Book" : "Add Book"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={newBook.title}
+                onChange={handleInputChange}
+                placeholder="Enter title"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Author</Form.Label>
+              <Form.Control
+                type="text"
+                name="author"
+                value={newBook.author}
+                onChange={handleInputChange}
+                placeholder="Enter author"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Publish Year</Form.Label>
+              <Form.Control
+                type="text"
+                name="publishYear"
+                value={newBook.publishYear}
+                onChange={handleInputChange}
+                placeholder="Enter publish year"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Edition</Form.Label>
+              <Form.Control
+                type="text"
+                name="edition"
+                value={newBook.edition}
+                onChange={handleInputChange}
+                placeholder="Enter edition"
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="text"
+                name="price"
+                value={newBook.price}
+                onChange={handleInputChange}
+                placeholder="Enter price"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={resetForm}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={addOrUpdateBook}>
+            {isEditMode ? "Save Changes" : "Add Book"}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
 
 export default Dashboard;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../src/assests/styles/login.css";
 
 function Register() {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [users, setUsers] = useState([]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (userData.password === userData.confirmPassword) {
+      setUsers((prevUsers) => [...prevUsers, userData]);
+      console.log("User registered:", userData);
+      setUserData({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      });
+    } else {
+      console.log("Passwords do not match");
+    }
+  };
+
   return (
     <div className="container-fluid p-3 my-5 d-flex justify-content-center">
       <div className="login-container row w-100 justify-content-center">
@@ -12,48 +45,64 @@ function Register() {
             alt="Phone image"
           />
         </div>
-
         <div className="col-12 col-md-6">
           <h1 className="login" style={{ textAlign: "center" }}>
             Register
           </h1>
 
-          <div className="form-group mb-4">
-            <label htmlFor="formControlLg">Username</label>
-            <input
-              type="email"
-              className="form-control form-control-lg"
-              id="formControlLg"
-            />
-          </div>
-          <div className="form-group mb-4">
-            <label htmlFor="formControlLg">Email</label>
-            <input
-              type="email"
-              className="form-control form-control-lg"
-              id="formControlLg"
-            />
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group mb-4">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                className="form-control form-control-lg"
+                id="username"
+                name="username"
+                value={userData.username}
+                onChange={handleChange}
+              />
+            </div>
 
-          <div className="form-group mb-4">
-            <label htmlFor="formControlLg">Password</label>
-            <input
-              type="password"
-              className="form-control form-control-lg"
-              id="formControlLg"
-            />
-          </div>
-          <div className="form-group mb-4">
-            <label htmlFor="formControlLg">Confirm Password</label>
-            <input
-              type="password"
-              className="form-control form-control-lg"
-              id="formControlLg"
-            />
-          </div>
-          <button className="btn btn-primary mb-4 w-100 btn-lg">
-            Register
-          </button>
+            <div className="form-group mb-4">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                className="form-control form-control-lg"
+                id="email"
+                name="email"
+                value={userData.email}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group mb-4">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                className="form-control form-control-lg"
+                id="password"
+                name="password"
+                value={userData.password}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group mb-4">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                className="form-control form-control-lg"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={userData.confirmPassword}
+                onChange={handleChange}
+              />
+            </div>
+
+            <button className="btn btn-primary mb-4 w-100 btn-lg" type="submit">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
